60c161316a7a0f333283810882708df4741149a7
dukesilverest <dukesilverest@gmail.com>
2023-09-17 20:40:43 -0400

    styling typo

--- src/components/SupabaseLogin.tsx
+++ src/components/SupabaseLogin.tsx
@@ -23,6 +23,7 @@ export default function SupabaseLogin() {
             default: {
               colors: {
                 brand: primaryColor,
+                brandAccent: primaryColor,
               },
             },
           },
